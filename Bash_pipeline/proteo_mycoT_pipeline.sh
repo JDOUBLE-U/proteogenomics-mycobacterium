17423106073c4a517cc24ba09b5c8d142dffed83
Jan Willem Wijnands <janwillem.wijnands@gmail.com>
2015-03-30 13:15:06 -0700

    Update proteo_mycoT_pipeline.sh

--- Bash_pipeline/proteo_mycoT_pipeline.sh
+++ Bash_pipeline/proteo_mycoT_pipeline.sh
@@ -27,9 +27,18 @@ seq_file_1="/home/BPSYS/motifs_test.fasta"
 #Seq-jpeg
 seqLogo="/home/BPSYS/seqLogo.jpeg"
 
+#This command is for using Comet. This program analyze the mzXML-files.
+/home/janwillem/Desktop/comet_binaries_2015011/comet.2015011.linux.exe MM.mzXML -Pcomet.params.new -DMycobacterium_marinum_strain_ATCC_BAA_535.fasta
+
+#This command is PeptideProphet
+runprophet interact.htm
+
+#This command is ProteinProphet.
+#usage:	ProteinProphet.pl '<interact pep prob html file1><interact pep prob html file2>....' <outfile> (ICAT) (GLYC) (XPRESS) (ASAP_PROPHET) (ACCURACY) (ASAP) (REFRESH) (DELUDE) (NOOCCAM)
+perl ProteinProphet.pl
+
+
 
 #This python-script makes sequence logo's.
 python "/home/BPSYS/s1071701/WebLogo.py" ${seq_file_1} ${seqLogo}
-
-/home/janwillem/Desktop/comet_binaries_2015011/comet.2015011.linux.exe MM.mzXML -Pcomet.params.new -DMycobacterium_marinum_strain_ATCC_BAA_535.fasta
 
